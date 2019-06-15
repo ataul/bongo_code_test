49e1b5b23ba78585946e1145f8f818c7ef65ac5c
ataul <ataul.haque@gmail.com>
2019-06-15 10:31:02 +0600

    problem3 complete

--- problem3.php
+++ problem3.php
@@ -1,11 +1,31 @@
 <?php
 function lca($node1, $node2) {
+	$ancestor1 = ancestors($node1);
+	$ancestor2 = ancestors($node2);
+	foreach ($ancestor1 as $value1) {
+		foreach ($ancestor2 as $value2) {
+			if($value1==$value2){
+				echo "Least common ancestor of $node1->value and $node2->value is $value1";
+				return;
+			}
+		}
+	}
+}
 
+function ancestors($node,$ancestors = array()){
+	if($node->parent==null){
+		if(sizeof($ancestors)==0){
+			return array($node->value);
+		}else{
+			return $ancestors;
+		}
+	}else{
+		$ancestors[] = $node->parent->value;		
+		return ancestors($node->parent,$ancestors);
+	}
 }
 
 class Node {
-	$value;
-	$parent;
 	public function __construct($value,$parent) {
 		$this->value = $value;
 		$this->parent = $parent;
@@ -22,4 +42,6 @@ $six	= new Node(6,$three);
 $seven	= new Node(7,$three);
 $eight	= new Node(8,$four);
 $nine	= new Node(9,$four);
+
+lca($six,$seven);
 ?>
